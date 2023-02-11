a6e4c680b23f047f610c0ddeadd1d31f32968825
dpMelian <astrodaniel1234@gmail.com>
2023-02-11 00:24:47 +0000

    feat: add type badge

---
+++ client/src/components/TypeBadge.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import styled from "styled-components";
+import { firstLetterToUpperCase } from "../helpers/firstLetterToUpperCase.ts";
+import { TYPES } from "../constants/pokemonTypes.ts"; 
+
+interface BadgeProps {
+    type: string;
+};
+
+const Badge = styled.span<BadgeProps>`
+    border-radius: 5px;
+    background-color: ${props => TYPES[props.type]};
+    padding: 0 1rem;
+`;
+
+interface Props {
+    type: string;
+}
+
+const TypeBadge = ({ type }: Props) => (
+    <Badge type={type}>
+        {firstLetterToUpperCase(type)}
+    </Badge>
+)
+
+export default TypeBadge
