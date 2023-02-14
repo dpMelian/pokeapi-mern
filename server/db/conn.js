a600b96d9a74e692ddb40019aa802e500165a370
dpMelian <astrodaniel1234@gmail.com>
2023-02-14 22:23:53 +0000

    chore: update collection name

--- server/db/conn.js
+++ server/db/conn.js
@@ -11,7 +11,7 @@ export const connectToServer = (callback) => {
   client.connect(function (err, db) {
     // Verify we got a good "db" object
     if (db) {
-      _db = db.db("user");
+      _db = db.db("trainer");
       console.log("Successfully connected to MongoDB.");
     }
     return callback(err);
