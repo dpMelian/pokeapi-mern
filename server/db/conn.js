5e86983673a208eecd20c1438917f686617bf151
dpMelian <astrodaniel1234@gmail.com>
2023-02-14 22:19:31 +0000

    feat: add MongoDB database

---
+++ server/db/conn.js
@@ -0,0 +1,23 @@
+import { MongoClient } from "mongodb";
+const database = "mongodb://localhost:27017/pokeapi-db";
+const client = new MongoClient(database, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+var _db;
+
+export const connectToServer = (callback) => {
+  client.connect(function (err, db) {
+    // Verify we got a good "db" object
+    if (db) {
+      _db = db.db("user");
+      console.log("Successfully connected to MongoDB.");
+    }
+    return callback(err);
+  });
+};
+
+export const getDb = () => {
+  return _db;
+};
