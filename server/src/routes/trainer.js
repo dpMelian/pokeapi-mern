5df13ae5379c53e598fd0e7c2a31860d56e87d3f
dpMelian <astrodaniel1234@gmail.com>
2023-02-19 23:19:36 +0000

    feat(server): add token request validation

--- server/src/routes/trainer.js
+++ server/src/routes/trainer.js
@@ -1,10 +1,20 @@
 import express from "express"
+import jsonwebtoken from "jsonwebtoken"
 import Trainer from "../models/Trainer.js"
 
 const trainerRoutes = express.Router()
+const SECRET_JWT_CODE = process.env.SECRET_JWT_CODE
+
+const blacklistedTokens = new Set()
 
 trainerRoutes.route("/trainer/add").post(function (req, res) {
-  const newTrainer = new Trainer(req.body)
+  const { name, email, password } = req.body
+  if (password.length < 8) {
+    return res
+      .status(400)
+      .json({ message: "Password must be 8 or more characters" })
+  }
+  const newTrainer = new Trainer({ name, email, password })
   newTrainer.save(function (err, trainer) {
     if (err) throw err
     res.json(trainer)
@@ -23,9 +33,66 @@ trainerRoutes.route("/trainer/login").post(function (req, res) {
         return res.status(401).json({ message: "Invalid email or password" })
       }
 
-      return res.json(trainer)
+      const token = jsonwebtoken.sign(
+        { id: trainer._id, email: trainer.email },
+        SECRET_JWT_CODE
+      )
+      return res.json({ success: true, token: token })
     })
   })
 })
+
+trainerRoutes.use(checkTokenBlacklist, (req, res, next) => {
+  if (req.path !== "/trainer/login" && req.path !== "/trainer/sign-up") {
+    next()
+  }
+})
+
+trainerRoutes.get("/trainer/name", (req, res) => {
+  const token = req.headers.authorization?.split(" ")[1]
+  if (!token) {
+    return res.status(401).json({ message: "No token provided" })
+  }
+
+  jsonwebtoken.verify(token, SECRET_JWT_CODE, async (err, decodedToken) => {
+    if (err) {
+      return res.status(403).json({ message: "Invalid token" })
+    }
+
+    try {
+      const trainer = await Trainer.findById(decodedToken.id)
+      const name = trainer.name
+      res.json({ name })
+    } catch (err) {
+      console.error(err)
+      res.status(500).send("Server Error")
+    }
+  })
+})
+
+trainerRoutes.delete("/trainer/logout", function (req, res) {
+  const token = req.headers.authorization?.split(" ")[1]
+  if (!token) {
+    return res.status(401).json({ message: "No token provided" })
+  }
+
+  blacklistedTokens.add(token)
+  console.log(blacklistedTokens)
+
+  return res.json({ success: true })
+})
+
+function checkTokenBlacklist(req, res, next) {
+  const token = req.headers.authorization?.split(" ")[1]
+  if (!token) {
+    return res.status(401).json({ message: "No token provided" })
+  }
+
+  if (blacklistedTokens.has(token)) {
+    return res.status(401).json({ message: "Token has been revoked" })
+  }
+
+  next()
+}
 
 export default trainerRoutes
