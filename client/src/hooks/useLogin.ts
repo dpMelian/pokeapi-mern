fb17e6a5fe3722eaff867b81a1cd43ae559a9d72
dpMelian <astrodaniel1234@gmail.com>
2023-02-19 23:21:22 +0000

    chore(client): add proper typing to useLogin

--- client/src/hooks/useLogin.ts
+++ client/src/hooks/useLogin.ts
@@ -1,14 +1,19 @@
-import axios from "axios"
-import { useMutation } from "react-query"
+import axios, { type AxiosResponse } from "axios"
+import { useMutation, type UseMutationResult } from "react-query"
 
 const api = axios.create({
   baseURL: "http://localhost:5000",
 })
 
-const useLogin = () => {
-  return useMutation(async (data: { email: string; password: string }) => {
-    await api.post("/trainer/login", data)
-  })
-}
+const useLogin = (): UseMutationResult<
+  AxiosResponse<any, any>,
+  unknown,
+  { email: string; password: string },
+  unknown
+> =>
+  useMutation(
+    async (data: { email: string; password: string }) =>
+      await api.post("/trainer/login", data)
+  )
 
 export default useLogin
