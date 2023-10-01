1255771fee88714d78debd35509b6896f253e2c2
dpMelian <astrodaniel1234@gmail.com>
2023-10-01 18:12:47 +0100

    refactor(client): use TW dark mode feature

--- client/src/providers/ThemeProviderWrapper.tsx
+++ client/src/providers/ThemeProviderWrapper.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react"
+import React, { useContext, useEffect } from "react"
 import { ThemeProvider } from "styled-components"
 import Context from "../contexts/DarkModeContext"
 import { DARK_MODE_THEME, THEME } from "../constants/theme"
@@ -9,6 +9,12 @@ const ThemeProviderWrapper = ({
   children: React.ReactNode
 }): JSX.Element => {
   const { isDarkMode } = useContext(Context)
+
+  useEffect(() => {
+    isDarkMode
+      ? document.body.classList.add("dark")
+      : document.body.classList.remove("dark")
+  }, [isDarkMode])
 
   return (
     <ThemeProvider theme={isDarkMode ? DARK_MODE_THEME : THEME}>
