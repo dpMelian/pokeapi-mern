e3ed196d84b70e49c4128b3a75f1517e711c7370
dpMelian <astrodaniel1234@gmail.com>
2023-11-03 11:06:29 +0000

    build(client): add shadcn components

--- client/src/components/LoadAndRenderImage.tsx
+++ client/src/components/LoadAndRenderImage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react"
+import { useEffect, useState } from "react"
 import Skeleton from "@mui/material/Skeleton"
 
 import useGetPokemonByName from "../hooks/useGetPokemonByName"
